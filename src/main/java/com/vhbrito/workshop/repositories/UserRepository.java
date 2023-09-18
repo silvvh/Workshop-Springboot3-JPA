853954b13be4cff8b38ba8bbd97154839ad293c3
Victor Brito <victorcyty@hotmail.com>
2023-09-18 12:31:03 -0300

    JPA repository, dependency injection (JPA, H2), database seeding

---
+++ src/main/java/com/vhbrito/workshop/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.vhbrito.workshop.repositories;
+
+import com.vhbrito.workshop.entities.User;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
