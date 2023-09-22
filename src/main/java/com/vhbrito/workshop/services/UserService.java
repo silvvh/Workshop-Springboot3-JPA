4ace1109bd1def25228e95c26ed31967bc43117a
Victor Brito <victorcyty@hotmail.com>
2023-09-22 11:12:56 -0300

    Delete user

--- src/main/java/com/vhbrito/workshop/services/UserService.java
+++ src/main/java/com/vhbrito/workshop/services/UserService.java
@@ -25,4 +25,8 @@ public class UserService {
     public User insertUser(User user) {
         return userRepository.save(user);
     }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
